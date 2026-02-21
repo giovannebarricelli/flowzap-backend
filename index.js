d0f1ea2083e43c9d5360d48fef1e1b46e55e4726
giovannebarricelli <giovannebarricellivml@gmail.com>
2026-02-21 00:36:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: { 
         headless: true,
-        args: [
-            '--no-sandbox', 
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--single-process'
-        ],
-        executablePath: '/usr/bin/google-chrome-stable' 
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
 });
 
@@ -26,9 +20,8 @@ client.on('ready', () => {
     console.log('Flowzap 2.0 CONECTADO COM SUCESSO!');
 });
 
-app.get('/', (req, res) => res.send('Servidor Flowzap Ativo!'));
+app.get('/', (req, res) => res.send('Servidor Ativo'));
 
-// Ajuste da Porta para o Render não dar Timeout
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Motor rodando na porta ${PORT}`);
